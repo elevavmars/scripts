b3f7dcb12aaafe56ac8199a4f0a638805426976d
elevavmars <140370057+elevavmars@users.noreply.github.com>
2024-10-20 00:15:48 +0100

    update full-rename.fish

--- full-rename.fish
+++ full-rename.fish
@@ -3,3 +3,6 @@ for file in *.txt
     mv $file (printf "newfile_%06d.txt" $i)
     set i (math $i + 1)
 end
+
+# renames the entire file name that has .txt extension
+# in a newfile_000001.txt, newfile_000002.txt... format 
