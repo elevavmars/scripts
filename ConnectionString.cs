113f5f12f954a7e3e36c9f5c0c3f10525a1ac5b5
elevavmars <140370057+elevavmars@users.noreply.github.com>
2024-12-28 02:08:17 +0000

    Update ConnectionString.cs

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -1,1 +1,1 @@
-string connectionString = @"Server=localhost,1433;Database=dbname;User Id=sa; Password=pass;";
+string connectionString = @"Server=localhost,1433;Database=dbname;User Id=sa; Password=pass;Trusted_Connection=False; TrustServerCertificate=True;";
